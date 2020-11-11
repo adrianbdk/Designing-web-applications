1e3d2e320c71213e1aff665c4d5eb4bf223bef58
Adrian Budka <226117@edu.p.lodz.pl>
2020-11-11 18:45:55 +0100

    car comparator done

---
+++ zad4/Car.java
@@ -0,0 +1,29 @@
+public class Car implements Comparable<Car>{
+    String brand;
+    String model;
+    int horsePower;
+    int productionYear;
+
+    public Car(String brand, String model, int horsePower, int productionYear){
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+        this.productionYear = productionYear;
+    }
+
+    @Override
+    public int compareTo(Car obj) {
+        if(this.horsePower != obj.horsePower)
+            return this.horsePower - obj.horsePower;
+        return this.productionYear - obj.productionYear;
+    }
+
+    public String toString(){
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", horsePower=" + horsePower + '\'' +
+                ", productionYear=" + productionYear +
+                '}';
+    }
+}
---
+++ zad4/CarComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class CarComparator implements Comparator<Car> {
+    @Override
+    public int compare(Car car1, Car car2) {
+        int carBrandComparison = car1.brand.compareTo(car2.brand);
+        if(carBrandComparison != 0)
+            return carBrandComparison;
+        return car1.model.compareTo(car2.model);
+    }
+}
